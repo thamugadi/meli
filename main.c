1670f9d6b7213e943f6c46ba74b1fccac0d86abf
aramya <22577625+thamugadi@users.noreply.github.com>
2022-06-13 10:47:45 +0100

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,7 @@ void main()
 
         print("Mel.\n", 10);
         print("Press any key to continue.\n\n", 13);
-        read_wait();
+        read_keyboard();
         init_gdt();
         init_32bit_paging();
         while(1);
