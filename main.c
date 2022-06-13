cb79e2c65bf96c0655388f834acca8ecf5b1baef
aramya <22577625+thamugadi@users.noreply.github.com>
2022-06-13 10:48:00 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,9 +4,9 @@ void main()
         free_vram();
         nextpage();
 
-
         print("Mel.\n", 10);
         print("Press any key to continue.\n\n", 13);
+        
         read_keyboard();
         init_gdt();
         init_32bit_paging();
