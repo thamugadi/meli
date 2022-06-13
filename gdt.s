214eca8740701002b5f742d82262b37fec3950f3
aramya <22577625+thamugadi@users.noreply.github.com>
2022-06-13 15:18:18 +0100

    Update gdt.s

--- gdt.s
+++ gdt.s
@@ -1,14 +1,15 @@
 .intel_syntax noprefix
 .global load_gdt
 load_gdt:
+cli
 mov eax, dword ptr [esp+4]
 lgdt [eax]
+jmp 0x8:longjump
+longjump:
 mov eax, 0x10
 mov ds, eax
 mov es, eax
 mov fs, eax
 mov gs, eax
 mov ss, eax
-jmp 0x8:.l_jump
-.l_jump:
 ret
