27d5fdbb8781ec3affdaaae4bce64d3f185c4f13
aramya <22577625+thamugadi@users.noreply.github.com>
2022-09-26 23:52:49 +0100

    macro syntax

--- 8259_PIC/pic.s
+++ 8259_PIC/pic.s
@@ -2,20 +2,20 @@
 .global init_pic
 .global end_interrupt
 
-.macro init_master icw4_needed cascade call_addr_interval8 level_triggered
+.macro .init_master icw4_needed cascade call_addr_interval8 level_triggered
 mov al, (\icw4_needed | (\cascade<<1) | (\call_addr_interval8<<2) | (\level_triggered<<3) | (1<<4))
 out 0x20, al
 .endm
-.macro init_slave icw4_needed cascade call_addr_interval8 level_triggered
+.macro .init_slave icw4_needed cascade call_addr_interval8 level_triggered
 mov al, (\icw4_needed | (\cascade<<1) | (\call_addr_interval8<<2) | (\level_triggered<<3) | (1<<4))
 out 0xA0, al
 .endm
 
-.macro remap_master_irq int
+.macro .remap_master_irq int
 mov al, \int
 out 0x21, al
 .endm
-.macro remap_slave_irq int
+.macro .remap_slave_irq int
 mov al, \int
 out 0xA1, al
 .endm
@@ -23,10 +23,10 @@ out 0xA1, al
 init_pic:
 push ebp
 mov ebp, esp
-init_master 0 0 0 0
-init_slave 0 0 0 0
-remap_master_irq 0x20
-remap_slave_irq 0x28
+.init_master 0 0 0 0
+.init_slave 0 0 0 0
+.remap_master_irq 0x20
+.remap_slave_irq 0x28
 push 15
 push offset text
 call kprint
