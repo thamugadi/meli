1ccc9aedd94c5c373a6f2cd20c8f77c8daf678c5
aramya <22577625+thamugadi@users.noreply.github.com>
2022-10-27 21:56:11 +0100

    todo: interrupt handler

--- interrupts/irq/irq.c
+++ interrupts/irq/irq.c
@@ -1,10 +1,14 @@
 void irq_handler(int irq)
 {
-	asm("pusha");
+	asm("mov ax, ss");
+	asm("mov ds, ax");
+	asm("mov es, ax");
+	asm("mov fs, ax");
+	asm("mov gs, ax");
 	char buf[4];
 	itoa(irq, buf);
 	kprint("IRQ ", 14);
 	kprint(buf, 14);
 	kprint("\n", 14);
-	while(1);
+	asm("jmp $");
 }
