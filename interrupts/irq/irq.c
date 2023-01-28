c634e7dbe9dded4371f9e7dbc669c7ffaa0c5976
aramya <22577625+thamugadi@users.noreply.github.com>
2023-01-28 02:13:23 +0100

    keyboard interrupt

--- interrupts/irq/irq.c
+++ interrupts/irq/irq.c
@@ -1,9 +1,18 @@
+unsigned char current_key;
+
 void irq_handler(int irq)
 {
 	char buf[4];
 	itoa(irq, buf);
-	kprint("IRQ ", 14);
-	kprint(buf, 14);
-	kprint("\n", 14);
+	if (irq)
+	{
+		kprint("IRQ ", 14);
+		kprint(buf, 14);
+		kprint("\n", 14);
+	}
 
+	if (irq == 1)
+	{
+		current_key = read_keyboard();
+	}
 }
