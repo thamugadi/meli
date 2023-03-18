e8a3e4512e31a68c8d7f6f64bde610ca4b4eb3dc
aramya <22577625+thamugadi@users.noreply.github.com>
2023-03-18 13:45:12 +0100

    todo

--- proc/task.c
+++ proc/task.c
@@ -1,6 +1,21 @@
 char processes[16] = {0};
 char processes_n = 1;
 extern int current_pid;
+
+typedef struct process_context
+{
+	unsigned int eip;
+	unsigned int eflags;
+	unsigned int eax;
+	unsigned int ecx;
+	unsigned int edx;
+	unsigned int ebx;
+	unsigned int esp;
+	unsigned int ebp;
+	unsigned int esi;
+	unsigned int edi;
+} process_context[16];
+
 void context_switch()
 {
 	if (processes_n == 1) return;
