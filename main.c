34aaf5884226240b294b5542342773f69a049186
aramya <22577625+thamugadi@users.noreply.github.com>
2023-03-18 13:37:24 +0100

    processes

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 int current_pid = 0;
 
-extern void user_test(void) __attribute__((section(".ring3")));
+extern void user_test(void) __attribute__((section(".ring3text")));
 #define DATA  0x40
 #define CODE  0x80
 #define STACK 0xa0
@@ -20,17 +20,19 @@ void main()
 
         init_gdt();
 
-	init_memory(0, 0x00,  0x00000000, 0x10000, KERNEL); 
 	init_idt();
 	init_pic();
 
-	init_memory(0, DATA,  0x10000000, 1, USER);
-        init_memory(0, CODE,  0x20000000, 1, USER);
-        init_memory(0, STACK, 0x28000000, 1, USER);
+        init_memory(0, 0x00, 0x00000000, 0x10000, KERNEL); 
+
 	change_directory(0);
 	enable_32bit_paging();
 
-	set_usermode(&user_test, 0x28000000); // run our first usermode program.
+	init_memory(0, DATA, 0x10000000, 1, USER);
+        init_memory(0, CODE, 0x10004000, 1, USER);
+        init_memory(0, STACK,0x10008000, 1, USER);
+
+	set_usermode(0x20000000, 0x28000000); // run our first usermode program.
 	
 	while(1);
 }
---
+++ proc/task.c
@@ -0,0 +1,15 @@
+char processes[16] = {0};
+char processes_n = 1;
+extern int current_pid;
+void context_switch()
+{
+	if (processes_n == 1) return;
+	
+	do
+	{
+		current_pid = (current_pid + 1) % 16;
+	} while(!(processes[current_pid]));
+
+
+        change_directory(current_pid);
+}
